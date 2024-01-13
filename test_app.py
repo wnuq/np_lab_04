ed6c4925762d0bc979de8afe96017efe2a1f32e1
wnuq <mwlipinski@gmail.com>
2024-01-13 19:25:00 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import unittest
 from app import add_numbers, subtract_numbers, multiply_numbers,divide_numbers
 class TestMathFunctions(unittest.TestCase):
-    def testAddNumbers(self):
+    def test_add_numbers(self):
         self.assertEqual(add_numbers(5, 3), 8)
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
